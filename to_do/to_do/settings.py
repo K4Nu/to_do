1fa1f734b6c0acf969559f5711aaf8f5e28cef4e
K4 <k4nu420@gmail.com>
2024-05-01 16:43:40 +0200

    postgre server

--- to_do/to_do/settings.py
+++ to_do/to_do/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG")
 
-ALLOWED_HOSTS = [".vercel.app"]
+ALLOWED_HOSTS = ["127.0.0.1", 'localhost']
 
 
 # Application definition
@@ -79,6 +79,8 @@ DATABASES = {
         'NAME': os.environ.get("DATABASE_NAME"),
         'USER': os.environ.get("DATABASE_USER"),
         'PASSWORD': os.environ.get("DATABASE_PASSWORD"),
+        "HOST":os.environ.get("DATABASE_HOST"),
+        "PORT":os.environ.get("DATABASE_PORT"),
     }
 }
 
